5c379f0ecfb49e3bb8e20cf967c000e40bb85d08
victor <55814035+m-vnio@users.noreply.github.com>
2024-09-19 18:12:17 -0500

    Add files via upload

---
+++ js/pelicula2All.js
@@ -0,0 +1,324 @@
+function all() {
+  const useThis = {
+    reactivity: {
+      datas: defineVal([]),
+      load: defineVal(true),
+    },
+    value: {
+      datas: [],
+    },
+    functions: {
+      fetch: (url = "") =>
+        `https://api-fetch.victor01sp.com/get.php?url=${encodeURIComponent(
+          url
+        )}`,
+    },
+  };
+
+  const $element = createNodeElement(`
+          <div class="div_1YmtSIZ">
+          
+              <header class="header_GxE7xRu">
+                  <h2 style="margin: auto 0">Peliculas</h2>
+              </header>
+  
+              <div class="div_xbSsVY0">
+                  <div id="divGenres" class="div_A1ZaHx" data-keydown-parent>
+                      <button class="focus" data-keydown="key-UOO23MW014kVXu" data-id="">Todos</button>
+                  </div>
+              </div>
+  
+              <div class="div_HR6EjtL scrollbar-y">
+                  <div id="itemNull" class="element-loader" style="--color:#ffffff; --pixel:60px"></div>
+                  <div id="itemFalse" class="div_4el4d3E" style="display: none;"></div>
+                  <div id="itemTrue" class="div_4el4d3E" style="display: none;" data-keydown-parent></div>
+              </div>
+  
+          </div>    
+      `);
+
+  const $elements = createObjectElement(
+    $element.querySelectorAll("[id]"),
+    "id",
+    true
+  );
+
+  const intersectionObserver = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          observer.unobserve(entry.target);
+          useThis.functions.dataLoad();
+        }
+      });
+    },
+    { root: null, rootMargin: "0px", threshold: 0 }
+  );
+
+  useThis.reactivity.load.observe((load) => {
+    elementDisplay($elements.itemNull, load);
+    elementDisplay($elements.itemFalse, !load && !useThis.value.datas.length);
+    elementDisplay($elements.itemTrue, !load && useThis.value.datas.length);
+  });
+
+  useThis.reactivity.datas.observe((datas) => {
+    const fragment = document.createDocumentFragment();
+    fragment.append(
+      ...datas.map((data) => {
+        // console.log(data);
+        const template = document.createElement("div");
+
+        template.innerHTML = `<a href="./pelicula2.html?id=${
+          data.TMDbId
+        }" class="a_YOLFLmd" data-keydown="key-qCVDDxQ4q1Wnr5o">
+          <picture class="picture_GJjPp2J"><img data-src="${`https://img.victor01sp.com/index.php?url=${encodeURIComponent(
+            data.images.poster.replace("/original/", "/w185/")
+          )}`}"></picture>
+          <span>${data.titles.name}</span>
+        </a>`;
+
+        IntersectionObserverImage.load(template.querySelector("img"), true);
+
+        return template.firstElementChild;
+      })
+    );
+
+    $elements.itemTrue.append(fragment);
+
+    if (datas.length == 24) {
+      intersectionObserver.observe(
+        $elements.itemTrue.children[
+          Math.floor($elements.itemTrue.children.length / 2)
+        ]
+      );
+    }
+  });
+
+  useThis.functions.dataLoad = () => {
+    const length = Math.floor($elements.itemTrue.children.length / 24) + 1;
+    const gender = $elements.divGenres
+      .querySelector("button.focus")
+      .getAttribute("data-id");
+
+    // const encodeQueryString = encodeQueryObject({
+    //   route: "/pelicula",
+    //   //   search: $elements.inputSearch.value.trim(),
+    //   category: $elements.divGenres
+    //     .querySelector("button.focus")
+    //     .getAttribute("data-id"),
+    //   start: length,
+    //   end: 50,
+    // });
+
+    // fetch(`https://api.victor01sp.com/iptv/api.php?${encodeQueryString}`)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     // useThis.value.datas = useThis.value.datas.concat(data);
+    //     // useThis.reactivity.datas.value = data;
+    //     // useThis.reactivity.load.value = false;
+    //     // $elements.loader.style.display = "none";
+    //     // $elements.items.style.display = "";
+    //     // functions.peliculas(json);
+    //   });
+
+    // `https://api-fetch.victor01sp.com/get.php?url=${ encode }`
+    //https://cuevana.biz/peliculas/page/1
+
+    fetchWebElement(
+      useThis.functions.fetch(
+        gender
+          ? `https://cuevana.biz/genero/${gender}/page/${length}`
+          : `https://cuevana.biz/peliculas/page/${length}`
+      )
+    ).then(($text) => {
+      const __NEXT_DATA__ = JSON.parse(
+        $text.querySelector("#__NEXT_DATA__").textContent
+      );
+
+      useThis.value.datas = useThis.value.datas.concat(
+        __NEXT_DATA__.props.pageProps.movies
+      );
+      
+      useThis.reactivity.datas.value = __NEXT_DATA__.props.pageProps.movies;
+      useThis.reactivity.load.value = false;
+
+      Array.from( $text.querySelectorAll('img') ).forEach( img => img.removeAttribute('src') )
+      // console.log(__NEXT_DATA__.props.pageProps.movies);
+    });
+  };
+
+  $elements.divGenres.addEventListener("click", (e) => {
+    const button = e.target.closest("button");
+    console.log(button);
+    if (button) {
+      Array.from($elements.divGenres.querySelectorAll("button")).forEach(
+        (button) => {
+          button.classList.remove("focus");
+        }
+      );
+      button.classList.add("focus");
+
+      $elements.itemTrue.innerHTML = "";
+      useThis.value.datas = [];
+      useThis.reactivity.load.value = true;
+      useThis.functions.dataLoad();
+    }
+  });
+
+  addEventListener("key-EeqSpNOJkulRszU", ({ detail }) => {
+    const e = detail.e;
+
+    const keydowns = Array.from(
+      document.querySelectorAll(`[data-keydown='${keys.array[keys.index]}']`)
+    );
+
+    const index = keydowns.findIndex((keydown) => keydown === focus.element);
+
+    if (e.key == "ArrowUp") {
+      if (keydowns[index - 1]) {
+        keydowns[index - 1].focus();
+      }
+    }
+
+    if (e.key == "ArrowDown") {
+      if (keydowns[index + 1]) {
+        keydowns[index + 1].focus();
+      }
+    }
+
+    if (e.key == "ArrowRight") {
+      if (keys.array[keys.index + 1]) {
+        const active =
+          document.querySelector(
+            `[data-keydown='${keys.array[keys.index + 1]}'].active`
+          ) ||
+          document.querySelector(
+            `[data-keydown='${keys.array[keys.index + 1]}']`
+          );
+
+        if (active) active.focus();
+      }
+    }
+  });
+
+  addEventListener("key-UOO23MW014kVXu", ({ detail }) => {
+    const e = detail.e;
+
+    const keydowns = Array.from(
+      document.querySelectorAll(`[data-keydown='${keys.array[keys.index]}']`)
+    );
+
+    const index = keydowns.findIndex((keydown) => keydown === focus.element);
+
+    if (e.key == "ArrowRight") {
+      if (keydowns[index + 1]) {
+        keydowns.forEach((keydown) => keydown.classList.remove("focus"));
+        keydowns[index + 1].classList.add("focus");
+        keydowns[index + 1].focus();
+      }
+    }
+
+    if (e.key == "ArrowLeft") {
+      if (keydowns[index - 1]) {
+        keydowns.forEach((keydown) => keydown.classList.remove("focus"));
+        keydowns[index - 1].classList.add("focus");
+        keydowns[index - 1].focus();
+      } else {
+        const active =
+          document.querySelector(`[data-keydown='${keys.array[0]}'].active`) ||
+          document.querySelector(`[data-keydown='${keys.array[0]}']`);
+
+        if (active) active.focus();
+      }
+    }
+
+    if (e.key == "ArrowDown") {
+      const active =
+        document.querySelector(
+          `[data-keydown='${keys.array[keys.index + 1]}'].active`
+        ) ||
+        document.querySelector(
+          `[data-keydown='${keys.array[keys.index + 1]}']`
+        );
+
+      if (active) active.focus();
+    }
+  });
+
+  addEventListener("key-qCVDDxQ4q1Wnr5o", ({ detail }) => {
+    const e = detail.e;
+
+    const num = getColumnCount(focus.parent, 10, 10);
+
+    console.log(focus.parent);
+
+    const keydowns = Array.from(
+      document.querySelectorAll(`[data-keydown='${keys.array[keys.index]}']`)
+    );
+
+    const index = keydowns.findIndex((keydown) => keydown === focus.element);
+
+    if (e.key == "ArrowRight") {
+      if (keydowns[index + 1]) {
+        keydowns[index + 1].focus();
+      }
+    }
+
+    if (e.key == "ArrowLeft") {
+      if (keydowns[index - 1] && index % num != 0) {
+        keydowns[index - 1].focus();
+      } else {
+        const active =
+          document.querySelector(`[data-keydown='${keys.array[0]}'].active`) ||
+          document.querySelector(`[data-keydown='${keys.array[0]}']`);
+
+        if (active) active.focus();
+      }
+    }
+
+    if (e.key == "ArrowDown") {
+      if (keydowns[index + num]) {
+        keydowns[index + num].focus();
+      }
+    }
+
+    if (e.key == "ArrowUp") {
+      if (keydowns[index - num]) {
+        keydowns[index - num].focus();
+      } else {
+        if (keys.array[keys.index - 1]) {
+          const active =
+            document.querySelector(
+              `[data-keydown='${keys.array[keys.index - 1]}'].active`
+            ) ||
+            document.querySelector(
+              `[data-keydown='${keys.array[keys.index - 1]}']`
+            );
+
+          if (active) active.focus();
+        }
+      }
+    }
+  });
+
+  fetch("./json/categoria_pelicula2.json")
+    .then((res) => res.json())
+    .then((json) => {
+      const fragment = document.createDocumentFragment();
+      fragment.append(
+        ...json.map((data) => {
+          return createNodeElement(`
+        <button data-keydown="key-UOO23MW014kVXu" data-id="${data
+          .split(" ")
+          .join("-")
+          .toLowerCase()}">${data}</button>
+        `);
+        })
+      );
+
+      $elements.divGenres.append(fragment);
+    });
+
+  useThis.functions.dataLoad();
+  return $element;
+}
---
+++ js/pelicula2One.js
@@ -0,0 +1,243 @@
+function one(id) {
+  const useThis = {
+    reactivity: {
+      datas: defineVal([]),
+      load: defineVal(true),
+    },
+    value: {
+      datas: [],
+    },
+    functions: {
+      fetch: (url = "") =>
+        `https://api-fetch.victor01sp.com/get.php?url=${encodeURIComponent(
+          url
+        )}`,
+    },
+  };
+
+  const $element = createNodeElement(`
+                <div class="div_1YmtSIZ">
+        
+                    <picture class="picture_HsVx30S">
+                        <img style="display: none;" id="backdrop" src="" alt="">
+                    </picture>
+        
+                    <header class="header_GxE7xRu">
+                        <header class="header_GxE7xRu header_uxi6Edf">
+        
+                          <button class="button_mzTN7sc" data-keydown="key-RfwNgjwVeLgKee7" onclick="history.back()"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-svg-name="fi fi-rr-angle-left"><path d="M17.17,24a1,1,0,0,1-.71-.29L8.29,15.54a5,5,0,0,1,0-7.08L16.46.29a1,1,0,1,1,1.42,1.42L9.71,9.88a3,3,0,0,0,0,4.24l8.17,8.17a1,1,0,0,1,0,1.42A1,1,0,0,1,17.17,24Z"></path></svg></button>
+                          <div class="div_gKpyXFL">
+                            <button id="buttonPlay" class="button_mzTN7sc" data-keydown="key-RfwNgjwVeLgKee7"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-svg-name="fi fi-rr-play"><path d="M20.494,7.968l-9.54-7A5,5,0,0,0,3,5V19a5,5,0,0,0,7.957,4.031l9.54-7a5,5,0,0,0,0-8.064Zm-1.184,6.45-9.54,7A3,3,0,0,1,5,19V5A2.948,2.948,0,0,1,6.641,2.328,3.018,3.018,0,0,1,8.006,2a2.97,2.97,0,0,1,1.764.589l9.54,7a3,3,0,0,1,0,4.836Z"></path></svg></button>
+                          </div>
+        
+                      </header>
+                    </header>
+                  
+                    <div class="div_HR6EjtL">
+                        <div id="itemNull" class="element-loader" style="--color:#ffffff; --pixel:60px"></div>
+                        <div id="itemFalse" class="div_4el4d3E" style="display: none;"></div>
+                        <div id="itemTrue" class="div_33qcuHa" style="display: none;" data-keydown-parent>
+                          <div class="div_G4EThnx">
+                              <picture class="picture_rC8z3Zj">
+                                  <img id="image" src="" alt="">
+                              </picture>
+                              <div>
+                                  <h3 id="title"></h3>
+                                  <p id="sinopsis"></p>
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+        
+                </div>    
+            `);
+
+  const $elements = createObjectElement(
+    $element.querySelectorAll("[id]"),
+    "id",
+    true
+  );
+
+  // const intersectionObserver = new IntersectionObserver(
+  //   (entries, observer) => {
+  //     entries.forEach((entry) => {
+  //       if (entry.isIntersecting) {
+  //         observer.unobserve(entry.target);
+  //         useThis.functions.dataLoad();
+  //       }
+  //     });
+  //   },
+  //   { root: null, rootMargin: "0px", threshold: 0 }
+  // );
+
+  useThis.reactivity.load.observe((load) => {
+    elementDisplay($elements.itemNull, load);
+    elementDisplay($elements.itemFalse, !load && !useThis.value.datas.length);
+    elementDisplay($elements.itemTrue, !load && useThis.value.datas.length);
+    elementDisplay($elements.backdrop, !load && useThis.value.datas.length);
+  });
+
+  useThis.reactivity.datas.observe((data) => {
+    if (Object.keys(data).length) {
+      console.log(data);
+
+      $elements.backdrop.src = `https://img.victor01sp.com/index.php?url=${encodeURIComponent(
+        data.images.poster.replace("/original/", "/w342/")
+      )}`;
+
+      $elements.image.src = `https://img.victor01sp.com/index.php?url=${encodeURIComponent(
+        data.images.poster.replace("/original/", "/w342/")
+      )}`;
+      $elements.title.textContent = data.titles.name;
+      $elements.sinopsis.textContent = data.overview;
+    }
+  });
+
+  useThis.functions.dataLoad = () => {
+    const length = $elements.itemTrue.children.length;
+
+    // const encodeQueryString = encodeQueryObject({
+    //   route: "/pelicula",
+    //   //   search: $elements.inputSearch.value.trim(),
+    //   //   category: $elements.divGenres
+    //   //     .querySelector("button.focus")
+    //   //     .getAttribute("data-id"),
+    //   start: length,
+    //   end: 50,
+    // });
+
+    // console.log(encodeQueryString);
+
+    // fetch(
+    //   "https://api-fetch.victor01sp.com/get.php?url=" +
+    //     encodeURIComponent(
+    //       `http://tvlatino.club:2082/player_api.php?username=MLKP90SAZs&password=4Z3KWMe7GnW2&action=get_vod_info&vod_id=${id}`
+    //     )
+    // )
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     useThis.value.datas = useThis.value.datas.concat(data);
+    //     useThis.reactivity.datas.value = data;
+    //     useThis.reactivity.load.value = false;
+    //   });
+
+    fetchWebElement(
+      useThis.functions.fetch(`https://cuevana.biz/pelicula/${id}/${id}`)
+    ).then(($text) => {
+      const __NEXT_DATA__ = JSON.parse(
+        $text.querySelector("#__NEXT_DATA__").textContent
+      );
+
+      useThis.value.datas = useThis.value.datas.concat(
+        __NEXT_DATA__.props.pageProps.thisMovie
+      );
+      useThis.reactivity.datas.value = __NEXT_DATA__.props.pageProps.thisMovie;
+      useThis.reactivity.load.value = false;
+
+      Array.from($text.querySelectorAll("img")).forEach((img) => {
+        img.removeAttribute("src");
+        img.removeAttribute("srcset");
+      });
+    });
+    //
+  };
+
+  $elements.buttonPlay.addEventListener("click", () => {
+    const countArray = (array) => (array.length ? array : false);
+
+    const videos =
+      countArray(useThis.reactivity.datas.value.videos.latino) ||
+      countArray(useThis.reactivity.datas.value.videos.spanish) ||
+      countArray(useThis.reactivity.datas.value.videos.english) ||
+      countArray(useThis.reactivity.datas.value.videos.japanese) ||
+      [];
+
+    const video = videos.find((video) => video.cyberlocker == "streamwish");
+
+    fetchWebElement(useThis.functions.fetch(video.result)).then(($text) => {
+      Array.from($text.querySelectorAll("img")).forEach((img) => {
+        img.removeAttribute("src");
+        img.removeAttribute("srcset");
+      });
+
+      Array.from($text.querySelectorAll("script")).forEach((script) => {
+        const urls = extractURLs(script.innerHTML);
+        if (urls[0]) {
+          const url = urls[0].slice(0, urls[0].lastIndexOf("'"));
+
+          if (url) {
+            const content = Android.getPageContent(url);
+            const $content = document.createElement("div");
+            $content.innerHTML = content;
+
+            const script = Array.from($content.querySelectorAll("script")).find(
+              (script) => script.innerHTML.includes("eval")
+            );
+
+            if (script) {
+              const script2 = script.innerHTML.slice(
+                script.innerHTML.indexOf("}('") + 2,
+                script.innerHTML.lastIndexOf("))")
+              );
+
+              const final = new Function(
+                `const fn = (...p) => p; return fn(${script2})`
+              );
+
+              const validate = (p, a, c, k, e, d) => {
+                while (c--)
+                  if (k[c]) {
+                    p = p.replace(
+                      new RegExp("\\b" + c.toString(a) + "\\b", "g"),
+                      k[c]
+                    );
+                  }
+
+                const scriptFunction = new Function(
+                  `return ${p.slice(p.indexOf("{"), p.indexOf(");"))}`
+                );
+                return scriptFunction();
+              };
+
+              Android.openWithDefault(
+                validate(...final()).sources[0].file,
+                "video/*"
+              );
+            }
+          }
+        }
+      });
+      // console.log($text);
+    });
+
+    // Android.openWithDefault(
+    //   `http://tvlatino.club:2082/movie/MLKP90SAZs/4Z3KWMe7GnW2/${useThis.reactivity.datas.value.movie_data.stream_id}.${useThis.reactivity.datas.value.movie_data.container_extension}`,
+    //   "video/*"
+    // );
+  });
+
+  addEventListener("key-RfwNgjwVeLgKee7", ({ detail }) => {
+    const e = detail.e;
+
+    const keydowns = Array.from(
+      document.querySelectorAll(`[data-keydown='${keys.array[keys.index]}']`)
+    );
+
+    const index = keydowns.findIndex((keydown) => keydown === focus.element);
+
+    if (e.key == "ArrowRight") {
+      if (keydowns[index + 1]) {
+        keydowns[index + 1].focus();
+      }
+    }
+
+    if (e.key == "ArrowLeft") {
+      if (keydowns[index - 1]) {
+        keydowns[index - 1].focus();
+      }
+    }
+  });
+
+  useThis.functions.dataLoad();
+  return $element;
+}
